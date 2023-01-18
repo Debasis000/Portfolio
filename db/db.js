7c1e927ddb5ec0b64ceeb6af47243e7d240ff6ec
Debasis Dalai <106977966+Debasis000@users.noreply.github.com>
2023-01-18 15:09:56 +0530

    File Uploaded

---
+++ db/db.js
@@ -0,0 +1,318 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				"Hi &#128075;",
+				"I'm Vinay Somawat. I’m currently working as a Software Engineer at Biofourmis India specialized in JavaScript Applications. I did my undergrad in CS from NIT Warangal.",
+				"I'm a developer, geek and curious human besides being an OpenSource enthusiast. I have experience of delivering solutions for interesting problems from startup and industry space.",
+				"I love to read, listen to music, and travel.",
+			],
+		},
+		contact: {
+			text: [
+				"If you would like to get in touch with me, be it for exploring a technology, a business, or to just say hi, feel free to send me an email. My email address is: vinaysomawat40@gmail.com",
+			],
+		},
+	},
+	skills: [
+		{
+			title: "Languages",
+			skillName: "JavaScript",
+			color: "1",
+			percentage: "70",
+		},
+		{
+			title: "Frameworks/Libraries",
+			skillName: "Reactjs",
+			color: "6",
+			percentage: "50",
+		},
+		{
+			title: "Backend",
+			skillName: "Nodejs, MongoDB",
+			color: "2",
+			percentage: "40",
+		},
+		{
+			title: "Clouds",
+			skillName: "AWS(EC2, S3), Heroku, Netlify",
+			color: "3",
+			percentage: "30",
+		},
+		{
+			title: "Design",
+			skillName: "HTML, Bootstrap, SCSS",
+			color: "4",
+			percentage: "70",
+		},
+		{
+			title: "Version Control",
+			skillName: "GitHub, JIRA, Trello",
+			color: "7",
+			percentage: "70",
+		},
+		{
+			title: "Tools",
+			skillName: "Postman, Chrome DevTools",
+			color: "3",
+			percentage: "80",
+		},
+		{
+			title: "Saas products",
+			skillName: "CleverTap, FreshDesk",
+			color: "5",
+			percentage: "50",
+		},
+		{
+			title: "Editor",
+			skillName: "VS Code",
+			color: "6",
+			percentage: "70",
+		},
+	],
+	projects: {
+		web: [
+			{
+				projectName: "Programming Diaries",
+				image: "images/programmingdiaries.png",
+				summary:
+					"Developed a full stack blog application to provide content on techical topics across the internet with admin interface.",
+				preview: "https://programmingdiaries.herokuapp.com/",
+				techStack: [
+					"SQLite",
+					"Bootstrap",
+					"JavaScript",
+					"Heroku",
+				],
+			},
+			{
+				projectName: "Find Your Bank",
+				image: "images/findyourbank.png",
+				summary:
+					"Developed a React application to render a list of banks fetched from API. Filtered the banks based on queries from localstorage, marked favorites banks.",
+				preview: "https://clever-fermi-0d5d76.netlify.app",
+				techStack: ["Reactjs", "Bootstrap", "JavaScript", "Netlify"],
+			},
+			{
+				projectName: "Web Portfolio",
+				image: "images/portfolio.png",
+				summary:
+					"Web Portfolio to showcase acadmics, skills, projects and contact details in better manner.",
+				preview: "https://github.com/vinaysomawat/Travographer-Portal",
+				techStack: ["HTML", "Bootstrap", "JavaScript"],
+			},
+			{
+				projectName: "Resume Builder",
+				image: "images/resume-builder.png",
+				summary:
+					"Browser based editor to build and download Resumes in a customizable templates.",
+				preview: "https://vinaysomawat.github.io/Resume-Builder",
+				techStack: ["HTML", "Bootstrap", "JavaScript"],
+			},
+		],
+		software: [
+			{
+				projectName: "Pizza Ordering ChatBot",
+				image: "images/pizzaorderchatbot.png",
+				summary:
+					"ChatBot using Dialogflow, Firebase database which stores the chat data in the realtime database.",
+				preview:
+					"https://github.com/vinaysomawat/Pizza-Ordering-ChatBot",
+				techStack: ["Dailogflow", "Firebase"],
+			},
+			{
+				projectName: "WhatsApp-Bot",
+				image: "images/whatsappbot.jpg",
+				summary:
+					"Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
+				preview: "https://github.com/vinaysomawat/WhatsApp-Bot",
+				techStack: ["Selenium", "Chrome Webdriver", "Python"],
+			},
+			{
+				projectName: "Bill Generator",
+				image: "images/billgenerator.png",
+				summary:
+					"GUI to transfer data to excel sheets and generate bills on the local shops.",
+				preview: "https://github.com/vinaysomawat/Bill-Generator",
+				techStack: ["Tkinter", "Openxlpy", "Python"],
+			},
+		],
+		android: [
+			{
+				projectName: "NITW-CSE",
+				image: "images/nitwcse.jpg",
+				summary:
+					"The Application display details of Department courses, reference books, research, publication and faculty profile.",
+				preview: "https://github.com/vinaysomawat/NITW-CSE",
+				techStack: ["JAVA", "XML", "Android"],
+			},
+			{
+				projectName: "CareerHigh-App",
+				image: "images/carrerhigh.png",
+				summary:
+					"The Application display the webpages of website careerhigh.in in android devices.",
+				preview: "https://github.com/vinaysomawat/CareerHigh-Android",
+				techStack: ["JAVA", "XML", "Android"],
+			},
+		],
+		freelance: [
+			{
+				projectName: "SnylloAir.com",
+				image: "images/snylloair.png",
+				summary:
+					"Developed a company website to showcase the purpose, services and products provided by the company to audience.",
+				preview: "https://www.snylloair.com/",
+				techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
+			},
+			{
+				projectName: "Delivery+",
+				image: "images/AM-Logo-.png",
+				summary:
+					"Android Application to display website in android devices.",
+				preview:
+					"https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
+				techStack: ["Android", "JAVA", "Play Store"],
+			},
+		],
+	},
+	experience: [
+		{
+			title: "Netflix-Clone",
+			duration: "May  - June, 2022",
+			subtitle: "",
+			details: [
+				"Working on the products in the healthcare/digital therapeutics domain."
+			],
+			tags: [
+				"JavaScript",
+				"",
+				"Bootstrap",
+				"Nodejs",
+				"Jenkins",
+			],
+			icon: "heartbeat",
+		},
+		{
+			title: "Novopay Solutions Pvt. Ltd.",
+			duration: "June 2020 - April 2022",
+			subtitle: "Software Engineer",
+			details: [
+				"Implemented Aadhaar Enabled Payment services such as Bio-metric eKYC, Cash Withdrawal, Balance Enquiry, Mini-Statements, and Money transfer; completed more than 20 story points in each sprint.",
+				"Integrated QR Code and reduced the effective time by 50 percent to load money into wallet, Clevertap events to track user actions, Freshdesk ticketing service and chat-bot services. Worked on user onboarding, approval, and finance interfaces.",
+				"Co-ordinated closely with the product team, backend team, android team, and QA team to deliver the product builds before deadlines.",
+			],
+			tags: [
+				"JavaScript",
+				"Angular",
+				"React",
+				"Bootstrap",
+				"Nodejs",
+				"Jenkins",
+			],
+			icon: "qrcode",
+		},
+		{
+			title: "ThinkPedia LLP",
+			duration: "May 2019 - June 2019",
+			subtitle: "SDE Intern",
+			details: [
+				"Worked as a full stack developer to support tech team.",
+				"Developed a customer Web Application from scratch for social media management.",
+			],
+			tags: ["JavaScript", "Angular", "Bootstrap", "Java", "Spring Boot"],
+			icon: "group",
+		},
+	],
+	education: [
+		{
+			title: "Master in Computer Application",
+			duration: "2 year",
+			subtitle: "Biju Pattnaik University of Technology, Odisha",
+			details: [
+				
+			],
+			tags: [
+				"Internet of Things",
+				"Operating Systems",
+				"Database Management System",
+				"Advanced Computer Networks",
+				"Compiler Designing",
+				"Cloud Computing",
+			],
+			icon: "graduation-cap",
+		},
+		{
+			title: "Bachelore of Computer Application",
+			duration: "3 year",
+			subtitle: "Berhampur University, Odisha",
+			details: [
+				
+			],
+			tags: [
+				"Computer Fundamentals",
+				"Operating Systems",
+				"Database Management System",
+				"Data Stracutre and Algorithms",
+				
+			],
+			icon: "graduation-cap",
+		},
+	],
+	footer: [
+		{
+			label: "Dev Profiles",
+			data: [
+				{
+					text: "Stackoverflow",
+					link: "https://stackoverflow.com/users/8461233/vinay-somawat",
+				},
+				{
+					text: "GitHub",
+					link: "https://github.com/vinaysomawat",
+				},
+				{
+					text: "LeetCode",
+					link: "https://leetcode.com/somawatvinay/",
+				},
+			],
+		},
+		{
+			label: "Resources",
+			data: [
+				{
+					text: "Enable Dark/Light Mode",
+					func: "enableDarkMode()",
+				},
+				{
+					text: "Print this page",
+					func: "window.print()",
+				},
+				
+			],
+		},
+		{
+			label: "Social Profiles",
+			data: [
+				{
+					text: "Linkedin",
+					link: "https://www.linkedin.com/in/vinaysomawat/",
+				},
+				{
+					text: "Twitter",
+					link: "https://twitter.com/thesigmakid",
+				},
+				{
+					text: "Buy me a coffee",
+					link: "https://www.buymeacoffee.com/r194dME8y",
+				},
+			],
+		},
+		{
+			label: "copyright-text",
+			data: [
+				"Made with &hearts; by Debasis Dalai.",
+				"&copy; No Copyrights. Feel free to use this template."
+			],
+		},
+	],
+};
